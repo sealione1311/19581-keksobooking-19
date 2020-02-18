8a12e16564f553e8e5d4bab94d34a1c2f4d01e46
sealione <sealione@gmail.com>
2020-02-18 23:43:34 +0300

    module6-task2

--- js/form.js
+++ js/form.js
@@ -8,31 +8,31 @@
   var textOneRoom = 'Можно выбрать 1 гостя';
   var textTwoRooms = 'Можно выбрать 1 или 2 гостя';
   var textThreeRooms = 'Можно выбрать 1, 2 или 3 гостей';
-  var textHundredRooms= 'Выбирете вариант "не для гостей"';
+  var textHundredRooms = 'Выбирете вариант "не для гостей"';
 
   var matchRoomsAndGuests = function () {
     var roomNumber = parseInt(roomSelect.value, 10);
     var capacity = parseInt(guestSelect.value, 10);
     switch (true) {
-    case (roomNumber === 1 && capacity !== 1):
-      roomSelect.setCustomValidity(textOneRoom);
-      roomsError.textContent = textOneRoom;
-      break;
-    case ((roomNumber === 2 && capacity === 3) || (roomNumber === 2 && capacity === 0)):
-      roomSelect.setCustomValidity(textTwoRooms);
-      roomsError.textContent = textTwoRooms;
-      break;
-    case (roomNumber === 3 && capacity === 0):
-      roomSelect.setCustomValidity(textThreeRooms);
-      roomsError.textContent = textThreeRooms;
-      break;
-    case (roomNumber === 100 && capacity !== 0):
-      roomSelect.setCustomValidity(textHundredRooms);
-      roomsError.textContent = textHundredRooms;
-      break;
-    default:
-      roomSelect.setCustomValidity('');
-      roomsError.textContent = '';
+      case (roomNumber === 1 && capacity !== 1):
+        roomSelect.setCustomValidity(textOneRoom);
+        roomsError.textContent = textOneRoom;
+        break;
+      case ((roomNumber === 2 && capacity === 3) || (roomNumber === 2 && capacity === 0)):
+        roomSelect.setCustomValidity(textTwoRooms);
+        roomsError.textContent = textTwoRooms;
+        break;
+      case (roomNumber === 3 && capacity === 0):
+        roomSelect.setCustomValidity(textThreeRooms);
+        roomsError.textContent = textThreeRooms;
+        break;
+      case (roomNumber === 100 && capacity !== 0):
+        roomSelect.setCustomValidity(textHundredRooms);
+        roomsError.textContent = textHundredRooms;
+        break;
+      default:
+        roomSelect.setCustomValidity('');
+        roomsError.textContent = '';
     } roomsError.setAttribute('style', 'color: red');
   };
   roomSelect.addEventListener('change', matchRoomsAndGuests);
